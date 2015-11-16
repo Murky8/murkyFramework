f66e31dbaff4846dd834a8c61b8abd9c0754f980
murky8 <jq882@hotmail.com>
2015-11-16 09:31:45 +0000

    ...

--- murkyFramework/include/GfxDevice/version_gfxDevice.hpp
+++ murkyFramework/include/GfxDevice/version_gfxDevice.hpp
@@ -2,7 +2,20 @@
 //------------------------------------------------------------------------------
 // 2015 J. Coelho
 // Platform: Windows 32/64
+// note: partially depreciated as defines are now stored in project settings.
 
 //#define USE_OPENGL
-#define USE_DIRECT3D11
+//#define USE_DIRECT3D11
 //#define USE_DIRECT3D12
+
+#ifndef USE_OPENGL
+#ifndef USE_DIRECT3D11
+#ifndef USE_DIRECT3D12
+
+//stop compilation: at least one of these must be defined
+#error Gfx device version needs to be specified.
+
+#endif
+#endif
+#endif
+
