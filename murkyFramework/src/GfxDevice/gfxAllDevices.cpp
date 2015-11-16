6b0fe05e0bdc1124f986474dac4623c7b998a578
murky8 <jq882@hotmail.com>
2015-11-16 09:47:02 +0000

    seems to be working

--- murkyFramework/src/GfxDevice/gfxAllDevices.cpp
+++ murkyFramework/src/GfxDevice/gfxAllDevices.cpp
@@ -5,13 +5,7 @@
 #include <murkyFramework/include/version.hpp>
 #include <murkyFramework/include/GfxDevice/version_gfxDevice.hpp>
 
-// todo: move libs to solution folder
-#ifdef USE_DIRECT3D11
-#pragma comment(lib,"d3d11.lib")
-#pragma comment(lib,"d3dcompiler.lib")
-#endif // USE_DIRECT3D11
-
+// todo: remove 
 #ifdef USE_OPENGL
-#pragma comment(lib,"OpenGL32.lib")
 #include <external/glew/src/glew.c>   // note! This is a cpp file, not hpp.
 #endif // USE_OPENGL
